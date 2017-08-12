5e5dfd2e0159909623ee495fb7d76f2d862703dd
LaVaSHoCK <volkov.danrumu@yandex.ru>
2017-08-12 15:11:56 +0300

    1

--- Task-for-calculation/work_with_console.py
+++ Task-for-calculation/work_with_console.py
@@ -1,8 +1,32 @@
-from time import *
+import datetime
 
 #----------------print text-----------------
 
-print("Silence is golden")
+print("-Silence is golden\n")
 
 #----------------print date and name--------
+
+now = datetime.datetime.now()
+
+print (now.strftime("-%A \n-%B \n-DANIL \n"))
+
+#---------------print zerro-----------------
+
+catch = {1, 2, 3, 4, 5}
+
+for i in catch:
+    print("-","0"*i)
+
+#--------------print rectangle--------------
+
+print ("\n-","A"*5)
+print ("\n-","A"*5)
+print ("\n-","A"*5)
+print ("\n-","A"*5)
+print ("\n-","A"*5)
+print ("\n-","A"*5)
+print ("\n-","A"*5)
+print ("\n-","A"*5)
+
+#--------------print "W"--------------------
 
